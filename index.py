1a95dc96d5bafc5e61e660e94de645bd29ea5689
Lily Hoskin <lilyhoskin@gmail.com>
2014-09-22 19:01:53 +0100

    Added Hello Monkey test

--- index.py
+++ index.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/voice')
+def voice():
+    response = make_response(render_template("Response.xml"))
+    response.headers['Content-Type'] = "text/xml"
+    return response
+    
 if __name__ == '__main__':
     app.run()
